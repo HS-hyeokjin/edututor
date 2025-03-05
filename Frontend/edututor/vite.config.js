31dc782ca680283c484bc7fe3e4ef80d2c9b8eb5
HS-hyeokjin <90303458+HS-hyeokjin@users.noreply.github.com>
2025-03-05 13:09:39 +0900

    Update vite.config.js..

--- Frontend/edututor/vite.config.js
+++ Frontend/edututor/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         target: 'https://www.edututor.site',
         changeOrigin: true,
         secure: true,
-        rewrite: (path) => path.replace(/^\/api/, '') 
+        rewrite: (path) => path.replace(/^\/api/, '')  
       },
       '/ws': {
         target: 'wss://www.edututor.site', 
